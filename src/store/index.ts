d49ca2152e2fde47aa9e091023ac761f98cf51ef
Sofia <sofia.g.per@gmail.com>
2022-04-15 12:34:40 +0300

    init

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    APIURL: "http://localhost:3000/api/",
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
