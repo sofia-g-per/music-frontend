4d33ef59dfe73b8332b5fe767dd889e20b415bfe
Sofia <sofia.g.per@gmail.com>
2023-03-04 07:22:02 +0300

    fix/ Fixed file field not triggering on label press

--- src/components/UI/form/FileField.vue
+++ src/components/UI/form/FileField.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="form-field">
         <label :for="fieldData.name" class="main-btn main-btn--outline main-btn--with-icon">
-          <p class="main-btn--with-icon__text">{{fieldData.label}}</p>
+          <p class="main-btn--with-icon__text gradient-text">{{fieldData.label}}</p>
           <!-- <img :src="'@/assets/images/search_icon.svg'" aria-hidden> -->
         </label>
-        <Field :name="fieldData.name" type="file" v-bind="$attrs" v-slot="{handleChange, handleBlur}" aria-hidden>
-            <input type="file" class="file-input" @change="handleChange" @blur="handleBlur" />
+        <Field :name="fieldData.name"  type="file" v-bind="$attrs" v-slot="{handleChange, handleBlur}" aria-hidden>
+            <input :id="fieldData.name" type="file" class="file-input" @change="handleChange" @blur="handleBlur" />
         </Field>
         <p class="form-field__error-label" v-show="errorMessage">
           {{defaultError}}
@@ -28,12 +28,10 @@ export default defineComponent({
     }, 
     computed:{
         file(){
-            console.log(this.errorMessage)
             return this.errorMessage;
         }
     } ,
     setup(props) {
-        console.log(props)
         const { errorMessage } = useField(props.fieldData.name as string);
         return {
             errorMessage
